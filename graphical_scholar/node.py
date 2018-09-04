27ff3ac5170a2d14343a1b1f6c320192dac5ba06
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2018-09-04 14:00:48 +0530

    Models : Node, Author, Paper

---
+++ graphical_scholar/node.py
@@ -0,0 +1,56 @@
+"""
+    Network Models
+
+    o Node
+
+      o Article
+      o Author
+
+"""
+class Node(object):
+
+    def __init__(self, citations=0):
+        self.citations = citations
+
+class Article(Node):
+
+    def __init__(self, 
+            citations= 0, 
+            references= None, 
+            arxiv_id= None,
+            s2_id= None,
+            title= None,
+            year= None,
+            authors= None,
+            is_influential= None,
+            keywords= None,
+            query_phrase= None
+            ):
+        # attach parameters to instance
+        self.citations = citations
+        self.references = references
+        self.arxiv_id = arxiv_id
+        self.s2_id = s2_id
+        self.title = title
+        self.year = year
+        self.keywords = keywords
+        self.query_phrase = query_phrase
+        self.is_influential = is_influential
+        self.authors = []
+
+class Author(Node):
+
+    def __init__(self, 
+            citations= 0, 
+            arxiv_id= None,
+            s2_id= None,
+            gscholar_id= None,
+            keywords= None,
+            articles= None
+            ):
+        # attach parameters to instance
+        self.citations = citations
+        self.arxiv_id = arxiv_id
+        self.s2_id = s2_id
+        self.gscholar_id = s2_id
+        self.keywords = keywords
