f38772e204be6db05a1421629a92e1a44f0fd7ad
Suriyadeepan Ramamoorthy <suriyadeepan.r@gmail.com>
2018-09-04 15:34:38 +0530

    adds network model template

---
+++ graphical_scholar/network.py
@@ -0,0 +1,29 @@
+class Network(object):
+
+    def __init__(self, root=None):
+        # add root node
+        self.root = root
+
+    def up(self):
+        # update next level upward
+        #  collect references information
+        #    "Rise Up"
+        # 
+        #  o Multi-threaded query on references
+        if not root:
+            print(':: [Network] root node unavailable')
+            return
+
+        pass
+
+    def down(self):
+        # update next level downward
+        #  collect citation information
+        #    "Drill Down"
+        #
+        #  o Multi-threaded query on citations
+        if not root:
+            print(':: [Network] root node unavailable')
+            return
+
+        pass
